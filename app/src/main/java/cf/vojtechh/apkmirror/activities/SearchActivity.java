c20f6aaad8b98b79820205f9dc34b74a81605197
itachi1706 <kennethsohyq@gmail.com>
2019-05-09 21:28:24 +0800

    Migrated code to use AndroidX instead

--- app/src/main/java/cf/vojtechh/apkmirror/activities/SearchActivity.java
+++ app/src/main/java/cf/vojtechh/apkmirror/activities/SearchActivity.java
@@ -2,8 +2,8 @@ package cf.vojtechh.apkmirror.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.Window;
 import android.widget.Toast;
 
